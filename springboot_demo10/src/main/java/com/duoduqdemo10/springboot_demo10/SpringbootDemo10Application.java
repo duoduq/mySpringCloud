092334d30d655db44714110b639fb3205644ac03
duoduq <13695656613@163.com>
2019-01-16 19:29:16 +0800

    springboot集成AdminUi-client

---
+++ springboot_demo10/src/main/java/com/duoduqdemo10/springboot_demo10/SpringbootDemo10Application.java
@@ -0,0 +1,14 @@
+package com.duoduqdemo10.springboot_demo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo10Application.class, args);
+	}
+
+}
+
