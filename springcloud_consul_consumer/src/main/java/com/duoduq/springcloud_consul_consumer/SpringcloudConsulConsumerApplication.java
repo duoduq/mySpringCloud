9d559df51b0577aaa124c25891a2565ea536fbd1
duoduq <13695656613@163.com>
2019-01-23 19:13:18 +0800

    springcloud集成consul做注册中心

---
+++ springcloud_consul_consumer/src/main/java/com/duoduq/springcloud_consul_consumer/SpringcloudConsulConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.duoduq.springcloud_consul_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConsulConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConsulConsumerApplication.class, args);
+	}
+
+}
+
---
+++ springcloud_consul_consumer/src/test/java/com/duoduq/springcloud_consul_consumer/SpringcloudConsulConsumerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.duoduq.springcloud_consul_consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudConsulConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
