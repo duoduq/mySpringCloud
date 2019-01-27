8c0ad25f3c5f2438dc03e208732154bed1aa4936
duoduq <13695656613@163.com>
2019-01-27 16:46:47 +0800

    springcloud集成config配置中心git(Server)

---
+++ springcloud_config_server/src/main/java/com/duoduq/springcloud_config_server/SpringcloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.duoduq.springcloud_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringcloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigServerApplication.class, args);
+	}
+
+}
+
