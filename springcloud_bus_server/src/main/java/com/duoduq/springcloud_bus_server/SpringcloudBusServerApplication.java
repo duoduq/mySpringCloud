fc0d27265efae00405b68862524ca21acd1c66a4
duoduq <13695656613@163.com>
2019-01-28 01:08:27 +0800

    springcloud集成config,bus

---
+++ springcloud_bus_server/src/main/java/com/duoduq/springcloud_bus_server/SpringcloudBusServerApplication.java
@@ -0,0 +1,18 @@
+package com.duoduq.springcloud_bus_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class SpringcloudBusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudBusServerApplication.class, args);
+	}
+
+}
+
---
+++ springcloud_bus_server/src/test/java/com/duoduq/springcloud_bus_server/SpringcloudBusServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.duoduq.springcloud_bus_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudBusServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
